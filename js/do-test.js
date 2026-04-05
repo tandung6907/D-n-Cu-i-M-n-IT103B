02c3b11c762dd84a368485babe4940675ae6db28
NguyenTuanHung2007 <thwng08092007@gmail.com>
2026-04-05 14:29:04 +0700

    project

---
+++ js/do-test.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuBtn = document.getElementById('menuBtn');
+    const mobileMenu = document.getElementById('mobileMenu');
+
+    if(menuBtn) {
+        menuBtn.addEventListener('click', function() {
+            mobileMenu.classList.toggle('show');
+        });
+    }
+});
